07b60e2377c0e9a96593516e8acae69a175dca0e
Lido Hon <lidohon10@gmail.com>
2024-07-24 10:46:46 +0000

    hero section modified

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,4 @@
 import { FaLocationArrow } from 'react-icons/fa6';
-
 import MagicButton from './MagicButton';
 import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
@@ -29,7 +28,7 @@ const Hero = () => {
 			 *  0.2 to 0.03
 			 */}
 			<div
-				className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+				className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.01] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
 			>
 				{/* Radial gradient for the container to give a faded look */}
@@ -43,7 +42,7 @@ const Hero = () => {
 			<div className="flex justify-center relative my-20 z-10">
 				<div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
 					<p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-						Dynamic Web Magic with Next.js
+						Dynamic Web Designs with latest Frame works!
 					</p>
 
 					{/**
@@ -53,11 +52,12 @@ const Hero = () => {
 					 */}
 					<TextGenerateEffect
 						words="Transforming Concepts into Seamless User Experiences"
+						// words="Converting Ideas into Intuitive and Enjoyable User Experiences"
 						className="text-center text-[40px] md:text-5xl lg:text-6xl"
 					/>
 
 					<p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-						Hi! I&apos;m Lidet, a junior web developer.
+						Hello! I&apos;m Lidet, a junior web developer.
 					</p>
 
 					<a href="#about">
