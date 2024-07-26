78fa99451f71b6662fd801b7cd00490b2041c8f1
Lido Hon <lidohon10@gmail.com>
2024-07-26 11:33:40 +0000

    blah blah

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,7 +3,6 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-
 Sentry.init({
   dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
 
