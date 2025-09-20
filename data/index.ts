fbf8aa0e7fd004314364b9cac5ef75c172166352
LidoHon <lidohon10@gmail.com>
2025-09-20 16:51:35 +0300

    fix

--- data/index.ts
+++ data/index.ts
@@ -51,7 +51,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently Developing Abe's Garage: A Full Stack website.",
+    title: "Currently working as a Frontend Developer at Minab Tech",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
